ed274fc0bee8909b9ccb9f2375fbabfc7ace0692
fordij <WZBC4eMrf@G~>
2022-07-25 12:14:11 -0400

    Creating and deleting login stuff

--- mobile/src/main/java/com/example/airrandsredux/MainActivity.java
+++ mobile/src/main/java/com/example/airrandsredux/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.FileInputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,17 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //init textview
-        TextView textView = (TextView) findViewById(R.id.toDos);
+        TextView textView = findViewById(R.id.toDos);
 
         //init arraylist for goals (probably will make a sister array of booleans if they're done)
         ArrayList<String> tasks = new ArrayList<String>();
 
         //declare message box
-        EditText message = (EditText)findViewById(R.id.listInput);
+        EditText message = findViewById(R.id.listInput);
 
         //this handles the "create list" button on the home screen, updates the textview, and adds them to an arraylist
-        Button newList = (Button) findViewById(R.id.listButton);
-        newList.setOnClickListener( new View.OnClickListener(){
+        Button newList = findViewById(R.id.listButton);
+        newList.setOnClickListener(new View.OnClickListener(){
             public void onClick (View v){
                 //check if the input is empty, throw a toast if it is
                 if (message.getText().toString().compareTo("") == 0) {
@@ -50,23 +49,20 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-        ); //onclicklistener
+        );
 
-        /*create FTP objects
-        FTPClient client = new FTPClient();
-        FileInputStream fis = null;
-        String filename = "tasks.txt";
+        //go to the login screen
+        Button loginScreen = findViewById(R.id.signButton);
+        loginScreen.setOnClickListener(new View.OnClickListener(){
+                public void onClick (View v){
+                    setContentView(R.layout.activity_login);
+                }
+            }
+        );
 
-        //sending to watch via FTP
-        Button sendButton = (Button) findViewById(R.id.sendTo);
-        sendButton.setOnClickListener( new View.OnClickListener()
-                                       {
-                                           public void onClick (View v){
-                                               Log.d("Debug text",message.getText().toString());
-                                               uploadFTP.FTPup(tasks);
-                                           }
-                                       }
-        ); //onclicklistener
-         */
     }
+
+
+
+
 }
--- wear/src/main/java/com/example/airrandsredux/MainActivity.java
+++ wear/src/main/java/com/example/airrandsredux/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.example.airrandsredux.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -21,11 +24,52 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
 
+        //our arraylist
+        ArrayList<String> tasks = new ArrayList<String>();
+
+        //debug tasks, will replace with database grab
+        tasks.add("Debug 1");
+        tasks.add("Debug 2");
+        tasks.add("Debug 3");
+        tasks.add("Debug 4");
+        tasks.add("Debug 5");
+
+        //init switch
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        Switch switch2 = (Switch) findViewById(R.id.switch2);
+        Switch switch3 = (Switch) findViewById(R.id.switch3);
+        Switch switch4 = (Switch) findViewById(R.id.switch4);
+        Switch switch5 = (Switch) findViewById(R.id.switch5);
+
         //mTextView = binding.text;
+
+        //creating list
         Button sendButton = (Button) findViewById(R.id.getButton);
         sendButton.setOnClickListener( new View.OnClickListener(){
             public void onClick (View v){
-                Log.d("Debug text","work");
+
+                //each just sets text of a switch, makes it visible, and gives a debug log
+
+                switch1.setText(tasks.get(0));
+                switch1.setVisibility(View.VISIBLE);
+                Log.d("Debug tasks",tasks.get(0));
+
+                switch2.setText(tasks.get(1));
+                switch2.setVisibility(View.VISIBLE);
+                Log.d("Debug tasks",tasks.get(1));
+
+                switch3.setText(tasks.get(2));
+                switch3.setVisibility(View.VISIBLE);
+                Log.d("Debug tasks",tasks.get(2));
+
+                switch4.setText(tasks.get(3));
+                switch4.setVisibility(View.VISIBLE);
+                Log.d("Debug tasks",tasks.get(3));
+
+                switch5.setText(tasks.get(4));
+                switch5.setVisibility(View.VISIBLE);
+                Log.d("Debug tasks",tasks.get(4));
+
             }
         });
     }
